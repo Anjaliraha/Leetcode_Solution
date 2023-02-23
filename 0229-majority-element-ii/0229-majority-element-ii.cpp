11be5869930a4f83a62c18b5d0746340fa9863d6
Anjaliraha <54320622+Anjaliraha@users.noreply.github.com>
2023-02-23 17:24:32 +0530

    Time: 4 ms (99.00%), Space: 15.9 MB (59.40%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,18 +2,39 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
        vector<int>ans;
-        unordered_map<int,int>mp;
-        int n=nums.size();
-        int s=(n/3)+1;
-        for(int i=0;i<n;i++){
-            mp[nums[i]]++;
-        }
-        for(auto x:mp){
-            //cout<<x.first<<"->"<<x.second<<endl;
-            if(x.second>=s){
-                ans.push_back(x.first);
+        int candidate1=INT_MIN,candidate2=INT_MIN;
+        int life1=0,life2=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==candidate1){
+                life1++;
+            }
+            else if(nums[i]==candidate2){
+                life2++;
+            }
+            else if(life1==0){
+                candidate1=nums[i];
+                life1=1;
+            }
+            else if(life2==0){
+                candidate2=nums[i];
+                life2=1;
+            }
+            else{
+                life1--;
+                life2--;
             }
         }
+        int count1=0,count2=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==candidate1){
+                count1++;
+            }
+            if(nums[i]==candidate2){
+                count2++;
+            }
+        }
+        if(count1>(nums.size()/3))ans.push_back(candidate1);
+        if(count2>(nums.size()/3))ans.push_back(candidate2);
         return ans;
     }
 };
