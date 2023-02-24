53cd3ee58a3d97b5ebdf20986e671ca94a320d5e
Anjaliraha <54320622+Anjaliraha@users.noreply.github.com>
2023-02-25 01:30:03 +0530

    Time: 87 ms (89.40%), Space: 42.7 MB (44.75%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -4,18 +4,22 @@ public:
     int subarraySum(vector<int> &nums, int k)
     {
         int n = nums.size();
-        int count = 0, i = 0, j = 0;
+        int count = 0, j = 0;
         unordered_map<int, int> mp;
-        int sum=0;
-        mp[0]=1;//suppose mere paas ek array={4,5,6,7,8,-1,9} aur k =4 hai toh agar mai mp[0]=1 nhi karti hu aur sum calculate hu jo ki hai aayega sum = sum+num[0]=0+4=4 aur sum -k=4-4=0 toh kyuki 0 mere unordered map mai nhi hai toh iska count save nhi hoga aur hamara count galat ho jayega isliye hum mp[0]=1 kiye hai.
-        
+        vector<int>presum(nums.size(),0);
+        presum[0]=nums[0];
+       // int presum=0;
+        for(int i=1;i<nums.size();i++){
+            presum[i]=nums[i]+presum[i-1];
+        }
     while (j < nums.size())
         {
-            sum=sum+nums[j];
-            if(mp.find(sum-k)!=mp.end()){
-                count+=mp[sum-k];
+            if(presum[j]==k)count++;
+            //sum=sum+nums[j];
+            if(mp.find(presum[j]-k)!=mp.end()){
+                count+=mp[presum[j]-k];
             }
-            mp[sum]++;
+            mp[presum[j]]++;
             j++;
         }
         return count;
