d7f261829309b416024b552e87cd684801635234
Anjaliraha <54320622+Anjaliraha@users.noreply.github.com>
2023-02-25 03:48:23 +0530

    Time: 52 ms (61.13%), Space: 32.7 MB (5.17%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -12,7 +12,7 @@ public:
             sum+=nums[j];
             rem=sum%k;
             if(rem<0){
-                rem+=k;
+                rem+=k;//this is done because we want to make sure that the remainder remains positive like fo rcase such as [-1,2,9],k=2...we will get output as 1 if we take the neg number and proceed further so we need to make sure that we are making our remainder positive
             }
             if(rem==0)count++;
             if(mp.find(rem)!=mp.end())count+=mp[rem];
