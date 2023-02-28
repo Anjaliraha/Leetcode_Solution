825c3538e41ad7fdf92e49dc332ef10cb5cb5a5f
Anjaliraha <54320622+Anjaliraha@users.noreply.github.com>
2023-03-01 02:36:54 +0530

    Time: 5 ms (68.72%), Space: 8.9 MB (98.18%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -4,54 +4,36 @@ public:
         vector<char>s;
         int i=0,j=0;
         int n=chars.size();
+        int index=0;
         //int cnt=1;
         while(j<n)
         {
-            if(j<n-1 && chars[i]==chars[j]){
+            while(j<n && chars[i]==chars[j]){
                 j++;
                
             }
-            else 
-            {
-                s.push_back(chars[i]);
-               if(j-i>1 && j<=n-1 && chars[i]!=chars[j])
+            
+               chars[index++]=chars[i];
+               if(j-i>1 )
                 {
                    
                     for(auto x:to_string(j-i))
-                    s.push_back(x);
-                    i=j;
-                    if(j<n-1)j++;
+                    chars[index++]=x;
+                    
+                    
                 }
-                else if(j-i+1>1 && j==n-1 && chars[i]==chars[j])
-                {
-                   for(auto x:to_string(j-i+1))
-                    s.push_back(x);
-                    i=j;
-                    j++;
-                }
-                else if(j==n-1 && i==j){
-                    j++;
-                }
-                 else{
-                    i=j;
-                }
-            
+            i=j;
             }
-        }
+       // }
         
 
-        int ans = s.size();
-      
-        for(int i=0; i<ans; i++){
-           chars[i]=s[i];
-          
-        }
+       
 //    for(int i=0; i<chars.size(); i++){
 //            cout<<chars[i]<<" ";
           
 //         }
    
-        return ans;
+        return index;
        
     }
 };
