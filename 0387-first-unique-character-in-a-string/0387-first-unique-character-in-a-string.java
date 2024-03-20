8940a6de8751a811ae05c699c05e6846ad0f2415
Anjaliraha <54320622+Anjaliraha@users.noreply.github.com>
2024-03-20 10:51:05 +0530

    Time: 35 ms (11.77%), Space: 45.2 MB (32.64%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,24 +1,17 @@
 class Solution {
     public int firstUniqChar(String s) {
-        Character chr=null;
+       // Character chr=null;
         Map<Character,Integer>mp=new LinkedHashMap<>();
         for(int i=0;i<s.length();i++){
             mp.put(s.charAt(i),mp.getOrDefault(s.charAt(i),0)+1);
         }
        // System.out.println(mp);
-        for(Character ch:mp.keySet()){
-            if(mp.get(ch)==1){
-                chr=ch;
-                break;
-            }
-        }
-        if(chr==null)return -1;
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)==chr && chr!=null){
-                return i;
-               // break;
-            }
+           if(mp.get(s.charAt(i))==1){
+               return i;
+           }
         }
+       
         return -1;
     }
 }
